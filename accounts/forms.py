3a65d501ca2ce4011a6954d5bc6c833c9c915ca3
Ahmed Mansy <ahmedmansy265@gmail.com>
2021-09-09 13:52:20 +0200

    before sign in

---
+++ accounts/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import Account
+class RegistrationForm (forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+
+        }
+        ), required=True, max_length=40)
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs=
+            {
+        'placeholder': 'Confirm Password'
+            }
+        ), required=True, max_length=40)
+
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'phone number'
+        self.fields['email'].widget.attrs['placeholder'] = 'email'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
